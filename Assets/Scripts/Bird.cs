8c6cbd1c129b25173b482cc5ee6c756b037dbe7a
Angel <rodriguezaa@g.cofc.edu>
2017-06-28 00:49:14 -0400

    redo animation

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour {
+
+	public float upForce = 200f;
+
 	private bool isDead = false;
 	private Rigidbody2D rb2d;
 
@@ -14,12 +17,14 @@ public class Bird : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(isDead == false)
-		{
-			if (Input.GetMouseButtonDown (0)) 
-			{
+		if (isDead == false) {
+			if (Input.GetMouseButtonDown (0)) {
 				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce (new Vector2 (0, upForce));
 			}
-		
+		}	
+	}
+	void OnCollisionEnter2D(){
+		isDead = true;
 	}
 }
